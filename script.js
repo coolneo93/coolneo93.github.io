146aa4fd31951c2186cbd7c933b1f40065755b40
coolneo93 <78939135+coolneo93@users.noreply.github.com>
2023-08-08 15:42:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,62 @@ printButton.addEventListener('click', () => {
     printWindow.document.close();
     printWindow.print();
 });
+const tabsDiv = document.getElementById('tabs');
+const notepad = document.getElementById('notepad');
+const tabs = [];
+let selectedTabIndex = 0;
+
+function selectTab(index) {
+    selectedTabIndex = index;
+    notepad.value = tabs[selectedTabIndex];
+}
+
+function renderTabs() {
+    tabsDiv.innerHTML = '';
+    tabs.forEach((content, index) => {
+        const tabButton = document.createElement('button');
+        tabButton.textContent = `Tab ${index + 1}`;
+        tabButton.onclick = () => selectTab(index);
+        tabsDiv.appendChild(tabButton);
+    });
+}
+
+function saveTabsToLocalStorage() {
+    localStorage.setItem('tabs', JSON.stringify(tabs));
+}
+
+function loadTabsFromLocalStorage() {
+    const savedTabs = localStorage.getItem('tabs');
+    if (savedTabs) {
+        tabs.push(...JSON.parse(savedTabs));
+        renderTabs();
+        selectTab(0);
+    }
+}
+
+document.getElementById('new-tab').onclick = () => {
+    tabs.push('');
+    renderTabs();
+    selectTab(tabs.length - 1);
+};
+
+document.getElementById('save-tabs').onclick = () => {
+    tabs[selectedTabIndex] = notepad.value;
+    saveTabsToLocalStorage();
+};
+
+document.getElementById('clear-tabs').onclick = () => {
+    tabs.length = 0;
+    selectedTabIndex = 0;
+    notepad.value = '';
+    renderTabs();
+    localStorage.removeItem('tabs');
+};
+
+notepad.onkeyup = () => {
+    tabs[selectedTabIndex] = notepad.value;
+};
+
+// ... Add the print function here ...
+
+loadTabsFromLocalStorage();
