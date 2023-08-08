fbf637b9079512afc67c7e3fb9e1da1665c97c4f
notesgeek <78939135+coolneo93@users.noreply.github.com>
2023-08-08 16:05:52 -0700

    Update script.js
    Signed-off-by: notesgeek <78939135+coolneo93@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,49 +1,74 @@
+const tabsDiv = document.getElementById('tabs');
 const notepad = document.getElementById('notepad');
-const saveButton = document.getElementById('save');
-const clearButton = document.getElementById('clear');
-const titleInput = document.getElementById('title');
-const downloadButton = document.getElementById('download');
-const printButton = document.getElementById('print');
+const tabs = [];
+let selectedTabIndex = 0;
 
-// Load saved text if available
-if (localStorage.getItem('notepadText')) {
-    notepad.value = localStorage.getItem('notepadText');
+function selectTab(index) {
+    selectedTabIndex = index;
+    notepad.value = tabs[selectedTabIndex];
 }
 
-// Save the text to local storage
-saveButton.addEventListener('click', () => {
-    localStorage.setItem('notepadText', notepad.value);
-    alert('Saved!');
-});
+function renderTabs() {
+    tabsDiv.innerHTML = '';
+    tabs.forEach((content, index) => {
+        const tabDiv = document.createElement('div');
+        const tabButton = document.createElement('button');
+        const closeButton = document.createElement('button');
 
-// Clear the text and remove it from local storage
-clearButton.addEventListener('click', () => {
+        tabButton.textContent = `Tab ${index + 1}`;
+        closeButton.textContent = 'Close';
+
+        tabButton.onclick = () => selectTab(index);
+        closeButton.onclick = () => closeTab(index);
+
+        tabDiv.appendChild(tabButton);
+        tabDiv.appendChild(closeButton);
+        tabsDiv.appendChild(tabDiv);
+    });
+}
+
+function closeTab(index) {
+    tabs.splice(index, 1);
+    if (index === selectedTabIndex) {
+        selectedTabIndex = Math.max(0, index - 1);
+    }
+    renderTabs();
+    selectTab(selectedTabIndex);
+}
+
+function saveTabsToLocalStorage() {
+    localStorage.setItem('tabs', JSON.stringify(tabs));
+}
+
+function loadTabsFromLocalStorage() {
+    const savedTabs = localStorage.getItem('tabs');
+    if (savedTabs) {
+        tabs.push(...JSON.parse(savedTabs));
+        renderTabs();
+        selectTab(0);
+    }
+}
+
+document.getElementById('new-tab').onclick = () => {
+    tabs.push('');
+    renderTabs();
+    selectTab(tabs.length - 1);
+};
+
+document.getElementById('save-tabs').onclick = () => {
+    tabs[selectedTabIndex] = notepad.value;
+    saveTabsToLocalStorage();
+};
+
+document.getElementById('clear-tabs').onclick = () => {
+    tabs.length = 0;
+    selectedTabIndex = 0;
     notepad.value = '';
-    localStorage.removeItem('notepadText');
-});
+    renderTabs();
+    localStorage.removeItem('tabs');
+};
 
-// Download the text as a file with the given title
-downloadButton.addEventListener('click', () => {
-    const title = titleInput.value.trim() || 'untitled';
-    const content = notepad.value;
-    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `${title}.txt`;
-
-    // Append the anchor to the body
-    document.body.appendChild(a);
-    a.click();
-
-    // Remove the anchor from the body
-    document.body.removeChild(a);
-
-    URL.revokeObjectURL(url);
-});
-
-printButton.addEventListener('click', () => {
+document.getElementById('print').addEventListener('click', () => {
     const printWindow = window.open('', '_blank');
     printWindow.document.open();
     printWindow.document.write('<html><head><title>Print Notepad</title></head><body>');
@@ -52,3 +77,9 @@ printButton.addEventListener('click', () => {
     printWindow.document.close();
     printWindow.print();
 });
+
+notepad.onkeyup = () => {
+    tabs[selectedTabIndex] = notepad.value;
+};
+
+loadTabsFromLocalStorage();
