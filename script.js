46e5b5e8e9333d0ff8f6ce14edabd5a91bb80614
notesgeek <78939135+coolneo93@users.noreply.github.com>
2025-05-29 19:45:07 -0700

    Update script.js
    Signed-off-by: notesgeek <78939135+coolneo93@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const block = document.getElementById("block");
 const scoreDisplay = document.getElementById("score");
 const gameOverDiv = document.getElementById("gameOver");
 
-let gravity = -2;
+let gravity = -20;
 let isJumping = true;
 let velocity = 0;
 let score = 0;
@@ -16,7 +16,7 @@ const gameOverSound = new Audio("gameover.wav");
 
 function jump() {
   if (!gameRunning) return;
-  velocity = 20;
+  velocity = 2;
   jumpSound.currentTime = 0;
   jumpSound.play();
 }
